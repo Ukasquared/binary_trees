038977d7667ff42b772a6419077bb748f6807adc
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-06 02:25:35 +0100

    binary trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
 * binary_tree_insert_right - inserts a node as the left child of another node
-* parent - points to the parent node to insert left child in
-* value - value to store in the new node
+* @parent: points to the parent node to insert left child in
+* @value: value to store in the new node
 * Return: new node
 */
 
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete the entire tree
+ * @tree: points to the root
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+}
+
