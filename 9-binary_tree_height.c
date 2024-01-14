b4d3a5aa9a84db4282384e822ca82c4d8941c098
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-14 15:32:44 +0100

    calc the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,10 +10,14 @@ size_t traverse(binary_tree_t *tree);
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l, r, height = 0;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	return (1 + binary_tree_height(tree->right));
-	return (1 + binary_tree_height(tree->left));
+	r = (1 + binary_tree_height(tree->right));
+	l = (1 + binary_tree_height(tree->left));
+	height += (r > l ? r : l);
+	return (height);
 }
