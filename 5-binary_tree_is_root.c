d017c66013a576050d6338593b5282d6319976f9
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-14 13:56:25 +0100

    check if a gven node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,10 +5,12 @@
  * @node: check if node is root
  *
  * Return: 1 if true or 0 if false
-*/
+ */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0);
 	if (node->parent)
 		return (0);
 	return (1);
