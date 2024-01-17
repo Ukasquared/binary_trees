bcb2198ffbffe6fb7e2a33f66b068b7a4e9ad52b
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-17 04:08:51 +0100

    check if tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,6 +3,8 @@
 /**
  * binary_tree_is_perfect -  checks if a binary tree is full
  * @tree:  pointer to the root node of the tree to check
+ * @height: ...
+ * @level: ....
  * Return: 1 if true, 0 if false
 */
 
@@ -34,13 +36,15 @@ size_t height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	l_height = height(tree->left);
-	r_height= height(tree->right);
+	r_height = height(tree->right);
 	return (l_height > r_height ? l_height + 1 : r_height + 1);
 }
 
 /**
  * is_perfect - check if tree is perfect
  * @tree: pointer to the root node
+ * @height: .....
+ * @level: .....
  * Return: 1 if true otherwise 0
  */
 
@@ -51,7 +55,7 @@ size_t is_perfect(const binary_tree_t *tree, size_t height, size_t level)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (height == level ? 1: 0);
+		return (height == level ? 1 : 0);
 	l = is_perfect(tree->left, height, level + 1);
 	r = is_perfect(tree->right, height, level + 1);
 	return (l && r);
