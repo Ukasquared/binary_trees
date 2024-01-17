4301abf9e8e149935bde15ca2a82fabf387aeda1
Ukasquared <ukachukwujudith95@gmail.com>
2024-01-17 06:13:01 +0100

    calac balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,13 +27,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l_height, r_height;
+	size_t l_height, r_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    l_height = binary_tree_height(tree->left);
-    r_height = binary_tree_height(tree->right);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
-    return ((l_height > r_height ? l_height : r_height) + 1);
+	return ((l_height > r_height ? l_height : r_height) + 1);
 }
